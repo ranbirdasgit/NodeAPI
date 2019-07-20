ace89985b4619f2535d9eed4b1b1523ebc07ba2c
ranbirdasgit <das.ranbir@gmail.com>
2019-07-20 20:25:26 +0530

    Update db.js
    DB connection changes

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '',
-    database: 'cgcrrp_main'
+    database: ''
 });
 
 module.exports = connection;
